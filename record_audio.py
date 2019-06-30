9d7a8c2d08d6c6659d3e2258fdb6e06eda9c9ac2
Jared <jaredbbowden@gmail.com>
2019-06-30 15:53:05 -0500

    Update the audio recorder

--- record_audio.py
+++ record_audio.py
@@ -4,7 +4,8 @@ from pynput import keyboard
 import time
 import pyaudio
 import wave
-
+import sched
+import sys
 
 CHUNK = 8192
 FORMAT = pyaudio.paInt16
@@ -17,77 +18,70 @@ p = pyaudio.PyAudio()
 frames = []
 
 
-def callback(self, in_data, frame_count, time_info, status):
-    print("callback")
-    if self.key_pressed == True:
-        #stream_queue.put(in_data)
-        print("record")
-        frames.append(in_data)
-        return (in_data, pyaudio.paContinue)
-
-    elif self.key_pressed == False:
-        #stream_queue.put(in_data)
-        frames.append(in_data)
-        return (in_data, pyaudio.paComplete)
-
-    else:
-        print("not record")
-        return (in_data, pyaudio.paContinue)
+def callback(in_data, frame_count, time_info, status):
+    frames.append(in_data)
+    return (in_data, pyaudio.paContinue)
 
 
 class MyListener(keyboard.Listener):
     def __init__(self):
         super(MyListener, self).__init__(self.on_press, self.on_release)
         self.key_pressed = None
-
-        self.stream = p.open(format=FORMAT,
-                             channels=CHANNELS,
-                             rate=RATE,
-                             input=True,
-                             frames_per_buffer=CHUNK,
-                             stream_callback=self.callback)
-        print self.stream.is_active()
+        self.wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        self.wf.setnchannels(CHANNELS)
+        self.wf.setsampwidth(p.get_sample_size(FORMAT))
+        self.wf.setframerate(RATE)
 
     def on_press(self, key):
-        if key == keyboard.Key.cmd_l:
+        if key.char == 'r':
             self.key_pressed = True
+        return True
 
     def on_release(self, key):
-        if key == keyboard.Key.cmd_l:
+        if key.char == 'r':
             self.key_pressed = False
-
-    def callback(self, in_data, frame_count, time_info, status):
-        if self.key_pressed == True:
-            return (in_data, pyaudio.paContinue)
-        elif self.key_pressed == False:
-            return (in_data, pyaudio.paComplete)
-        else:
-            return (in_data, pyaudio.paAbort)
+        return True
 
 
 listener = MyListener()
 listener.start()
 started = False
+stream = None
 
-while True:
-    time.sleep(0.1)
-    if listener.key_pressed == True and started == False:
-        started = True
-        listener.stream.start_stream()
-        print "Start recording"
 
-    elif listener.key_pressed == False and started == True:
-        print "Stop recording"
-        listener.stream.stop_stream()
-        listener.stream.close()
+def recorder():
+    global started, p, stream, frames
+
+    if listener.key_pressed and not started:
+        # Start the recording
+        try:
+            stream = p.open(format=FORMAT,
+                            channels=CHANNELS,
+                            rate=RATE,
+                            input=True,
+                            frames_per_buffer=CHUNK,
+                            stream_callback=callback)
+            print("Stream active:", stream.is_active())
+            started = True
+            print("start Stream")
+        except:
+            raise
+
+    elif not listener.key_pressed and started:
+        print("Stop recording")
+        stream.stop_stream()
+        stream.close()
         p.terminate()
+        listener.wf.writeframes(b''.join(frames))
+        listener.wf.close()
+        print "You should have a wav file in the current directory"
+        sys.exit()
+    # Reschedule the recorder function in 100 ms.
+    task.enter(0.1, 1, recorder, ())
 
-        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
-        wf.close()
-        
-        # End
-        break
+
+print "Press and hold the 'r' key to begin recording"
+print "Release the 'r' key to end recording"
+task = sched.scheduler(time.time, time.sleep)
+task.enter(0.1, 1, recorder, ())
+task.run()
