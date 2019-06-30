3058a113f8814bcbeb77e196335a99fd5c73c571
Jared <jaredbbowden@gmail.com>
2019-06-30 11:12:30 -0500

    Simple working examples

---
+++ README.md
@@ -0,0 +1,10 @@
+# Text to speech  
+
+Some simple experiments to parse text from speech. Emphasis on libraries that allow us perform this operation _offline_.
+
+**References**
+https://github.com/Uberi/speech_recognition
+
+https://stackoverflow.com/questions/36523705/python-pocketsphinx-requesterror-missing-pocketsphinx-module-ensure-that-pocke
+
+https://realpython.com/python-speech-recognition/#using-record-to-capture-data-from-a-file
---
+++ example.py
@@ -0,0 +1,58 @@
+import sys
+#sys.path.insert(0, '/Users/Jared/Documents/speech_recognition/')
+sys.path.insert(0, '../speech_recognition/')
+import speech_recognition as sr
+from os import path
+
+
+######################################################
+# From microphone
+######################################################
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# recognize speech using Sphinx
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+
+
+######################################################
+# From file
+######################################################
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.record(source, duration=60)
+
+with open("test_sound_file3.wav", "wb") as f:
+    f.write(audio.get_wav_data())
+
+AUDIO_FILE = "./test_sound_file.wav"
+
+# use the audio file as the audio source
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  # read the entire audio file
+
+# recognize speech using Sphinx
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    the_text = r.recognize_sphinx(audio)
+    the_time = r.recognize_sphinx(audio, show_all=True)
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+
+# Quick google example
+r.recognize_google(audio)
+
+# Can we pull the timestamps?
+# https://stackoverflow.com/questions/37973541/segment-timestamps-in-pocketsphinx
+[(seg.word, seg.start_frame, seg.end_frame) for seg in the_time.seg()]
